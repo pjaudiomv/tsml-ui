abde880dfde9aec8fcec0df51c81c2efb52f38d1
pjaudiomv <34245618+pjaudiomv@users.noreply.github.com>
2026-08-27 13:43:30 -0400

    fix: switch default map tiles from Carto to OpenStreetMap (#551)

--- src/hooks/settings.tsx
+++ src/hooks/settings.tsx
@@ -83,8 +83,8 @@ export const defaults: TSMLReactConfig = {
       },
     },
     tiles: {
-      attribution: `&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>`,
-      url: 'https://{s}s.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png',
+      attribution: `&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors`,
+      url: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
     },
   },
   modes: ['search', 'location', 'me'],
